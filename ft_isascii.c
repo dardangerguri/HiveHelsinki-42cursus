5a5c849d83fe3d7e0fe35efdbd0de372d751162e
Dardan Gerguri <dgerguri@c3r1p7.hive.fi>
2023-06-12 16:27:44 +0300

    Character manipulation functions - Added comments

--- ft_isascii.c
+++ ft_isascii.c
@@ -6,9 +6,18 @@
 /*   By: dgerguri <dgerguri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/25 17:23:57 by dgerguri          #+#    #+#             */
-/*   Updated: 2022/11/17 16:21:10 by dgerguri         ###   ########.fr       */
+/*   Updated: 2023/06/11 22:08:46 by dgerguri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/**
+ * The ft_isascii function determines whether a given character is a valid ASCII character.
+ *
+ * This function checks if the character 'c' falls within the range of valid ASCII characters,
+ * which is from 0 to 127 (inclusive).
+ *
+ * It returns a non-zero value if the character is a valid ASCII character, and zero otherwise.
+ */
 
 int	ft_isascii(int c)
 {
