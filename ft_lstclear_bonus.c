612442ef5bda7b907509491ac63e3bd801fd52a1
Dardan Gerguri <dgerguri@c3r1p7.hive.fi>
2023-06-17 21:36:31 +0300

    Bonuses: Linked list functions - Added comments

--- ft_lstclear_bonus.c
+++ ft_lstclear_bonus.c
@@ -6,11 +6,25 @@
 /*   By: dgerguri <dgerguri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 19:41:14 by dgerguri          #+#    #+#             */
-/*   Updated: 2022/11/17 13:01:23 by dgerguri         ###   ########.fr       */
+/*   Updated: 2023/06/17 21:31:46 by dgerguri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+/**
+ * The ft_lstclear function is used to clear a linked list and free the memory occupied by its nodes.
+ * It takes in a double pointer to the head of the list, 'lst', and a pointer to a function, 'del',
+ * responsible for freeing the content of each node.
+ *
+ * The function checks if the 'lst' pointer is not NULL and enters a loop that continues
+ * until either the list becomes empty or the 'del' function pointer is NULL.
+ *
+ * Inside the loop, the 'del' function is called with the content of the current node, and then the current node is freed.
+ * The head pointer is updated to point to the next node.
+ *
+ * After the loop, '*lst' is set to 0 to indicate an empty list.
+*/
 
 void	ft_lstclear(t_list **lst, void (*del)(void*))
 {
--- ft_lstdelone_bonus.c
+++ ft_lstdelone_bonus.c
@@ -6,11 +6,20 @@
 /*   By: dgerguri <dgerguri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 19:02:50 by dgerguri          #+#    #+#             */
-/*   Updated: 2022/11/16 19:31:42 by dgerguri         ###   ########.fr       */
+/*   Updated: 2023/06/17 21:31:45 by dgerguri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+/**
+ * The ft_lstdelone function is used to delete a single node from a linked list and free the memory occupied by that node.
+ * It takes in a pointer to the node to be deleted, 'lst', and a pointer to a function, 'del',
+ * responsible for freeing the content of the node.
+ *
+ * The function checks if both the 'lst' pointer and the 'del' function pointer are not NULL.
+ * If so, it calls the 'del' function with the content of the node as the argument, and then frees the node itself.
+*/
 
 void	ft_lstdelone(t_list *lst, void (*del)(void*))
 {
